b7dd640153c1b4f60d62e5ecaff86c987775d204
Yizhuo Xie(Joy) <joylinkyizhuo@gmail.com>
2016-05-15 16:47:27 -0700

    A simple temperature demo

--- src/MaxTemperature.java
+++ src/MaxTemperature.java
@@ -1,9 +1,12 @@
+import org.apache.commons.configuration.*;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
 public class MaxTemperature {
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
@@ -13,6 +16,7 @@ public class MaxTemperature {
         Job job = new Job();
         job.setJarByClass(MaxTemperature.class);
         job.setJobName("Max temperature");
+
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         job.setMapperClass(MaxTemperatureMapper.class);
